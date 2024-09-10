caf9f0242edfb14c87d7917e24515dee974bc274
Tobia Marcucci <tobia.marcucci@gmail.com>
2024-09-10 12:44:51 -0700

    Adds a simple example.

---
+++ examples/hello_world.py
@@ -0,0 +1,20 @@
+import numpy as np
+from pybezier import BezierCurve
+
+# generate random curve
+np.random.seed(0)
+points = np.random.rand(5, 2)
+initial_time = 2
+final_time = 5
+curve = BezierCurve(points, initial_time, final_time)
+
+# plot curve with initial and final points
+import matplotlib.pyplot as plt
+plt.figure()
+curve.plot_trace_2d()
+times = [initial_time, final_time]
+for time in times:
+    value = curve(time)
+    plt.scatter(*value, c='r')
+    plt.text(*value, f"time = {time}")
+plt.show()
