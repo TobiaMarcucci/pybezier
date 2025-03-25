2b995bdc78daf6935b7fd933d87305829ff4f865
Tobia Marcucci <tobia.marcucci@gmail.com>
2025-03-24 20:31:29 -0700

    Updates version

--- pybezier/__init__.py
+++ pybezier/__init__.py
@@ -1,5 +1,5 @@
 from .bezier_curve import BezierCurve
 from .composite_bezier_curve import CompositeBezierCurve
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Tobia Marcucci"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pybezier',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple Python package to manipulate Bézier curves',
     url='https://github.com/TobiaMarcucci/pybezier',
     author='Tobia Marcucci',
